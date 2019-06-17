b538880edfcffbd5fcf6c6dd8272344680cf441c
wilku998 <wilkbartosz98@wp.pl>
2019-06-17 20:32:26 +0200

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const app = <p>app</p>
+
+render(app, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env) => {
+	const isProduction = env === 'production';
+	return {
+		entry: [ 'babel-polyfill', './src/app.tsx' ],
+		output: {
+			path: path.resolve(__dirname, 'public', 'dist'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					loader: 'babel-loader',
+				  },
+				  {
+					test: /\.js$/,
+					use: ["source-map-loader"],
+					enforce: "pre"
+				  },
+				{
+					test: /\.s?css$/,
+					use: [
+						isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+						'css-loader',
+						{
+							loader: 'sass-loader',
+							options: { sourceMap: true }
+						}
+					]
+				}
+			]
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		},
+		plugins: [
+
+		],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			publicPath: '/dist/',
+			historyApiFallback: true
+		}
+	};
+};
